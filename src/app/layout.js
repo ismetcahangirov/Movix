cd045fac005277cd405bc98d4ef07142adbd856c
ismetcahangirov <smitcrown4@gmail.com>
2025-12-16 23:29:06 +0400

    Rename layout.jsx to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
           type="image/svg+xml"
         />
 
-        <meta name="google-adsense-account" content="ca-pub-5882535517542517"/>
+        <meta name="google-adsense-account" content="ca-pub-5882535517542517" />
       </head>
       <body className={` font-montserrat antialiased bg-midnight flex flex-col justify-between min-h-screen`}>
         {children}
